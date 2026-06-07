5a32b4a4ee22b72a3cebb941ca904d033200a004
qoonzi <aprilana1105@gmail.com>
2026-06-07 18:57:17 +0700

    Update message text for 8th monthsary

--- message.js
+++ message.js
@@ -1,18 +1,16 @@
 const text = `
 
-Hai Uta ❤️
+Happy 8th Monthsary, Kakak 🤍
 
-makasih ya, buat semua hal kecil yang mungkin ga selalu keliatan tapi selalu aku rasain. 🥹🫂 makasih udah sabar ngadepin aku, makasih udah bertahan, makasih udah tetep ada walaupun kadang sikap aku bikin kamu cape. 
-aku tau, ga jarang aku bikin kamu kecewa, entah lewat kata-kata, sikap, atau cara aku bereaksi. and i’m really sorry for that. 
-maaf kalau aku belum selalu jadi versi yang kamu harapin, maaf kalau aku sering kebawa ego dan lupa ngeliat dari sudut pandang kamu. 
-please know this, none of it ever came from a place of not caring. justru karena aku peduli, kadang aku jadi berantakan sendiri. 
-thank you for not giving up easily, for choosing to stay, and for trying to understand me even when i’m hard to deal with. i’m learning, slowly, to be better not perfect, but better for you. 
-dan kalau kamu masih mau jalan bareng aku, i’ll keep trying to make you feel appreciated, heard, and loved the way you deserve. i don’t expect you to forget the times i messed up, and i’m not asking you to be okay instantly. 
-i just want you to know that every pelajaran yang datang dari rasa bersalah itu aku simpan baik-baik. aku lagi belajar buat lebih peka, lebih dengerin, dan ga langsung defensif waktu kamu kecewa. 
-aku sadar, rasa kecewa itu ga muncul tiba-tiba, pasti ada proses panjang yang aku kadang terlalu santai ngejalaninnya. makasih karena kamu masih mau jujur sama aku, masih mau ngomong walaupun mungkin capek. itu berarti banget buat aku. 
-dan kalau suatu hari nanti aku masih kepleset lagi, aku harap kamu tau aku ga pernah niat nyakitin kamu. aku cuma manusia yang masih belajar cara mencintai. untuk apapun itu, terima kasih sudah sabar sayang. ♥️
+Can't believe it's been 8 months already. Time really flies ya kalo dijalaninnya sama orang yang bikin hari-hari jadi lebih special (wlpun sambil prengat prengut banyak). Thank you for being part of my life, for all the laughs, random chats, little arguments, and all the moments we've shared together.
 
-❤️ Nana
+Aku tahu hubungan kita ga selalu perfect, but I really appreciate how we keep choosing each other every day. Makasih udah sabar polllll sama aku yang nyebelin ini, makasih udah dengerin cerita-cerita aku, dan selalu ada pas aku butuh. You make me feel loved, comfortable, and safe in a way that I never expected before.
+
+I hope we can keep growing together, understanding each other better, and creating more memories in the months ahead. No matter what happens, I want us to keep communicating, supporting, and loving each other.
+
+Once again, happy 8th mensiversary, Kakak sayang. Thank you for being you. I'm grateful to have you, and I can't wait to see what our next chapters will look like together. I love you always 🤍
+
+Love regards, yuyu.
 
 `;
 
